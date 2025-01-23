932eb01af7071452f134817f31abb89b522647a6
engulfy <xhaoyux@gmail.com>
2025-01-23 09:55:38 +0800

    Renamed bot, updated greet and exit

---
+++ src/main/java/Engulfy.java
@@ -0,0 +1,12 @@
+public class Engulfy {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Engulfy");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("\nBye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+
+
+    }
+}
