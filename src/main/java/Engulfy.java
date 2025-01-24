897af6daaa85016ed13e2bb999bff11c7548c53a
engulfy <xhaoyux@gmail.com>
2025-01-24 09:55:29 +0800

    Echo function added

--- src/main/java/Engulfy.java
+++ src/main/java/Engulfy.java
@@ -1,12 +1,26 @@
+import java.util.Scanner;
+
 public class Engulfy {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Engulfy");
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("\nBye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
 
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String userInput = sc.nextLine();
+            System.out.println("____________________________________________________________");
 
+            if (userInput.toLowerCase().equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                sc.close();
+                return;
+            } else {
+                System.out.println(userInput);
+                System.out.println("____________________________________________________________");
+                continue;
+            }
+        }
     }
 }
