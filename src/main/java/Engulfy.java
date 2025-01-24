2604bfdc0b3fbf28cf5f77fb0e83349a36cc2f99
engulfy <xhaoyux@gmail.com>
2025-01-24 11:30:49 +0800

    Added Mark as Done Functionality

--- src/main/java/Engulfy.java
+++ src/main/java/Engulfy.java
@@ -7,34 +7,79 @@ public class Engulfy {
         System.out.println("What can I do for you?");
 
         Scanner sc = new Scanner(System.in);
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         while (true) {
             String userInput = sc.nextLine();
+            String[] inputArr = userInput.split(" ", 2);
+            String instruction = inputArr[0];
+            String index = inputArr.length > 1 ? inputArr[1] : "";
             System.out.println("____________________________________________________________");
 
-            if (userInput.toLowerCase().equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.println("____________________________________________________________");
-                sc.close();
-                return;
-            } else if (userInput.toLowerCase().equals("list")) {
-                if (taskCount == 0) {
-                    System.out.println("No tasks yet!");
+            switch (instruction) {
+                case "bye":
+                    System.out.println("Bye. Hope to see you again soon!");
                     System.out.println("____________________________________________________________");
-                } else {
-                    for (int i = 0; i < taskCount; i++) {
-                        System.out.println((i + 1) + ". " + tasks[i]);
+                    sc.close();
+                    return;
+
+                case "list":
+                    if (taskCount == 0) {
+                        System.out.println("No tasks yet!");
+                        System.out.println("____________________________________________________________");
+                    } else {
+                        System.out.println("Here are the tasks in your list:");
+                        for (int i = 0; i < taskCount; i++) {
+                            System.out.println((i + 1) + ". " + tasks[i]);
+                        }
+                        System.out.println("____________________________________________________________");
                     }
+                    continue;
+
+                case "mark":
+                    try {
+                        int taskNumber = Integer.parseInt(index);
+                        if (taskNumber >= 1 && taskNumber <= taskCount) {
+                            tasks[taskNumber - 1].markAsDone();
+                            System.out.println("Nice! I've marked this task as done:");
+                            System.out.println(tasks[taskNumber - 1]);
+                            System.out.println("____________________________________________________________");
+                        } else {
+                            System.out.println("Task number out of range.");
+                            System.out.println("____________________________________________________________");
+
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.println("Please specify a valid task number.");
+                        System.out.println("____________________________________________________________");
+                    }
+                    continue;
+
+                case "unmark":
+                    try {
+                        int taskNumber = Integer.parseInt(index);
+                        if (taskNumber >= 1 && taskNumber <= taskCount) {
+                            tasks[taskNumber - 1].markAsNotDone();
+                            System.out.println("OK, I've marked this task as not done yet:");
+                            System.out.println(tasks[taskNumber - 1]);
+                            System.out.println("____________________________________________________________");
+                        } else {
+                            System.out.println("Task number out of range.");
+                            System.out.println("____________________________________________________________");
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.println("Please specify a valid task number.");
+                        System.out.println("____________________________________________________________");
+                    }
+                    continue;
+
+                default:
+                    tasks[taskCount] = new Task(userInput);
+                    taskCount++;
+                    System.out.println("added: " + userInput);
                     System.out.println("____________________________________________________________");
-                }
-            } else {
-                tasks[taskCount] = userInput;
-                taskCount++;
-                System.out.println("added: "+ userInput);
-                System.out.println("____________________________________________________________");
-                continue;
+                    continue;
             }
         }
     }
